85e70a6d4af532518acf19e7149097bb1ae291d8
ManjilS <manjil.shrestha2003@gmail.com>
2025-09-21 10:28:04 +0545

    en

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,33 @@
-//const express = require('express');
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
-const app = express();  
-const __dirname = path.resolve();
+
+const app = express();
+
+// Recreate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
- 
-//for deployment
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*",(_,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
-    });
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-  console.log('Server is running on port :'+ PORT);
+  console.log("Server is running on port :" + PORT);
 });
