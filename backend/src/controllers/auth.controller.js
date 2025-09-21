666e9b81c2dc6333aaf8710fadbd3a5bc6814ee8
ManjilS <manjil.shrestha2003@gmail.com>
2025-09-21 19:50:41 +0545

    login logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -60,3 +60,36 @@ export const signup= async(req,res)=>{
         res.status(500).json({message:"Internal Server Error"});
     }
 }
+
+export const login= async(req,res)=>{
+    const {email,password}=req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,
+            profilePic:user.profilePic,
+        });
+    }catch(err){
+        console.log("Error in login controller :",err);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+export const logout= async(_,res)=>{
+    res.cookie("jwt", "", {maxAge:0});
+    res.status(200).json({message:"Logged out successfully"});
+}
